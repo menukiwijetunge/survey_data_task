e19ca9dc1470e7028da5fee4de62a890b7928088
menukiwijetunge <menukiwijetunge@gmail.com>
2025-11-04 22:53:17 +1100

    Add parseData skeleton with overall logic plain

---
+++ survey.js
@@ -0,0 +1,26 @@
+function parseData(rawData) {
+    //TODO: Check the format of the input
+
+    //TODO: Seperate out into rows
+
+    /*
+     * TODO: Filter rows:
+     *      1. Exclude headers
+     *      2. Check submission time
+     *      3. Check Data types (should be integer (1 to 5) answer for each question)
+     */
+
+
+    /*
+     * TODO: Calculate and save results:
+     *      1. Form an array with the average to result for each question 
+     *       (question number identified by index)
+     *      2. Calculate the average for each question and store in the array 
+    */
+
+
+    /*
+     * TODO: DDisplay results
+     *      1. Print out question and corresponidng result from array
+    */
+}
